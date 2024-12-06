b76ace2abd5d730aebc916a58cef8cac721aa943
Heliarisoa Lucile Rakotondrabe <heliarisoa-lucile.rakotondrabe@etu.u-bordeaux.fr>
2024-12-06 08:00:44 +0100

    conflit gérer

--- app/inscription/page.tsx
+++ app/inscription/page.tsx
@@ -4,6 +4,7 @@ import { Navbar } from '@/components/navbar/navbar'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-blue-500 via-teal-400 to-blue-300 text-white">
+      <Navbar/>
       <InscriptionForm />
     </main>
   )
